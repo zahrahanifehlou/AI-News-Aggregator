0d324424fd56844728339fae37079c57f0cd7d1e
zahrahanifehlou <zahra.hanifehlou@ksilink.com>
2026-07-08 16:54:49 +0200

    change docker add ollama

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,10 @@
 
 echo "Waiting for Postgres..."
 while ! python -c "
-import socket
-import os
-import time
+import socket, os, time
 s = socket.socket()
 host = os.getenv('POSTGRES_HOST', 'postgres')
 port = int(os.getenv('POSTGRES_PORT', 5432))
-print(f'Waiting for Postgres at {host}:{port}...')
 s.connect((host, port))
 print('Postgres is ready!')
 " 2>/dev/null; do
@@ -17,8 +14,7 @@ done
 
 echo "Waiting for Redis..."
 while ! python -c "
-import socket
-import os
+import socket, os
 s = socket.socket()
 host = os.getenv('REDIS_HOST', 'redis')
 port = int(os.getenv('REDIS_PORT', 6379))
@@ -28,7 +24,18 @@ print('Redis is ready!')
   sleep 1
 done
 
-echo "All dependencies are ready. Starting application..."
+echo "Waiting for Ollama..."
+while ! python -c "
+import socket, os, time
+s = socket.socket()
+host = os.getenv('OLLAMA_HOST', 'ollama').replace('http://', '').replace('https://', '').split(':')[0]
+port = 11434
+print(f'Waiting for Ollama at {host}:{port}...')
+s.connect((host, port))
+print('Ollama is ready!')
+" 2>/dev/null; do
+  sleep 2
+done
 
-# Execute the command passed as arguments
+echo "All services are ready. Starting application..."
 exec "$@"
